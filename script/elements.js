1421d68893178b9b3eb36f45306a4b64d4dc30e9
Mathew Br <matvei_berezin@mail.ru>
2022-05-11 22:29:38 +0300

    add link's controller

--- script/elements.js
+++ script/elements.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
    paralax(document.getElementById('main'), 0.5);
    inpupFile(document.getElementById('exFile'));
-   buttonDownUp(document.querySelector('button'));
+   // let button = document.querySelector('button');
+   buttonDownUp(document.querySelectorAll('button'));
+   linksController(document.querySelectorAll('header a'));
 });
 
 
@@ -60,8 +62,228 @@ function inpupFile(elem) {
    });
 }
 
-function buttonDownUp(elem) {
-   elem.addEventListener('pointerdown', () => elem.classList.toggle('down'));
-   elem.addEventListener('pointerup', () => elem.classList.toggle('down'));
+function buttonDownUp(elems) {
+   if (!elems) return;
+   for (let elem of elems) {
+      if (!elem) continue;
+      elem.addEventListener('pointerdown', () => elem.classList.toggle('down'));
+      elem.addEventListener('pointerup', () => elem.classList.toggle('down'));
+   }
+}
+//
+async function linksController(kit) {
+   for (let a of kit) {
+      a.addEventListener('click', async (e) => {
+         e.preventDefault();
+
+         /**
+          * determine which page the click is on
+          * if the attribute value is an anchor
+          *       and we are on the home page, then rewind to find the element whose anchor is at the top with the specified offset
+          *       if wea are not on the home page
+          *             and there is no element with such an id on the page - message "change the href:value to the link for this element"
+          *             if the elemtnt is there but it is hidden - show it and rewind
+          * if the attribute value is an link, hide old content by assigning a sign of presence in the DOM
+          *       check if it was loaded earlier, if there is a hidden
+          *             if there is - display and rewind
+          *             if not - start the download, show process, display, rewind
+          */
+
+         let page = window.location.href;
+         // let page = window.location.pathname;
+         let href = getHref(a);
+         let offset = document.body.querySelector('header').clientHeight;
+         let changing = document.getElementById('changing');
+         let scroll = window.scrollY;
+
+         if (whatTypeHref(href) == 'ancor') {
+            let elem = document.getElementById(href.match(/\w+/));
+            if (elem) {
+               let position = elem.getBoundingClientRect().top + scroll - offset;
+               animate({
+                  duration: 1000,
+                  draw: function draw(progress) {
+                     let pos = position - scroll;
+                     window.scrollTo(0, scroll + pos * progress);
+                  },
+                  timing: function circ(timeFraction) {
+                     return 1 - Math.sin(Math.acos(timeFraction));
+                  }
+               });
+            } else {
+               if (changing.cash.loaded) {
+                  hidden(changing).then(() => {
+                     changing.innerHTML = changing.cash.oldContent;
+                     changing.removeAttribute('data-status');
+                     window.scrollTo(0, getPositionElem(changing, offset));
+                  }).then(() => {
+                     show(changing).then(() => {
+                        buttonDownUp(document.querySelectorAll('button'));
+                     });
+                  })
+               }
+            }
+         }
+         if (whatTypeHref(href) == 'link') {
+            // let changing = document.getElementById('changing');
+            if (!changing.cash || !changing.cash.loaded) {
+               let ancors = getAncorNewContent(a);
+               let content = await getContent(href);
+               let fragment = getFragment(content, '<body>', '</body>', false);
+               let elems = ancors.map((elem) => {
+                  return getElements(fragment, elem);
+               });
+               let newContent = document.createElement('div');
+               elems.forEach(elem => {
+                  newContent.append(elem);
+               });
+               cashContent(changing, changing.innerHTML, newContent.innerHTML);
+            }
+            if (changing.getAttribute('data-status') == 'loded') {
+               let position = changing.getBoundingClientRect().top + scroll - offset;
+               animate({
+                  duration: 1000,
+                  draw: function draw(progress) {
+                     let pos = position - scroll;
+                     window.scrollTo(0, scroll + pos * progress);
+                  },
+                  timing: function circ(timeFraction) {
+                     return 1 - Math.sin(Math.acos(timeFraction));
+                  }
+               });
+            } else {
+               hidden(changing).then(() => {
+                  changing.innerHTML = changing.cash.newContent;
+                  changing.dataset.status = 'loded';
+                  window.scrollTo(0, getPositionElem(changing, offset));
+               }).then(() => {
+                  show(changing).then(() => {
+                     buttonDownUp(document.querySelectorAll('button'));
+                  });
+               });
+            }
+         }
+      });
+   };
+
+   function cashContent(elem, oldContent, newContent) {
+      elem.cash = {loaded: true, oldContent: oldContent, newContent: newContent};
+   }
+
+   function getHref(a) {
+      if (a.tagName == 'A' && a.hasAttribute('href')) {
+         return a.getAttribute('href');
+      } else {
+         console.log('fn-> getHref, element is not teg "a" or heve not attribute "href"')
+      }
+   }
+
+   function getAncorNewContent(a) {
+      if (a.dataset.newcontent) {
+         let ancors = a.dataset.newcontent.split(' ');
+         return ancors;
+      } else {
+         console.log(`fn-> getAncorContetn ${a} has not data-newcontent attribute`)
+      }
+   }
+
+   function whatTypeHref(value) {
+      return /^#.*/.test(value) ? 'ancor' : 'link';
+   }
+
+   function getContent(url) {
+      return fetch(url).then(response => {
+         if (response.ok) {
+            return response.text();
+         } else {
+            console.log('request is failed in fn getContent ', response.status);
+            return;
+         }
+      }).then(text => {
+         return text;
+      }).catch(err => console.log(err));
+   }
+
+   //get html fragment from tag to tag (inclusive - default true, without <script> - default true) looking for the first match
+   function getFragment(str, from, to, inclusive = true, nosctipt = true) {
+      let regexp = new RegExp(`${from}(.*?)${to}`, `s`);
+      let without = /<script.*?>.*?<\/script>/gs;
+      let fragment = str.match(regexp);
+      if (!fragment) {
+         console.log('fn-> getFragment, no matches');
+         return;
+      }
+      if (inclusive === true) return nosctipt ? fragment[0].replace(without, '') : fragment[0];
+      else return nosctipt ? fragment[1].replace(without, '') : fragment[1];
+   }
+
+   function getElements(bunch, selector) {
+      let temp = document.createElement('div'); //temporary location to extract items
+      temp.innerHTML = bunch;
+      let element = temp.querySelector(`${selector}`);
+      temp = '';
+      return element;
+   }
+
+   function getPositionElem(elem, offset) {
+      if (elem) {
+         return elem.getBoundingClientRect().top + window.scrollY - offset;
+      } else {
+         return 0;
+      }
+   }
 }
 
+function animate({duration, draw, timing}) {
+   let start = performance.now();
+   requestAnimationFrame(function animate(time) {
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+      let progress = timing(timeFraction);
+      draw(progress);
+      if (timeFraction < 1) {
+         requestAnimationFrame(animate);
+      }
+   });
+}
+
+function animatePromise({duration, draw, timing}) {
+   return new Promise((resolve) => {
+      let start = performance.now();
+      requestAnimationFrame(function animate(time) {
+         let timeFraction = (time - start) / duration;
+         if (timeFraction > 1) timeFraction = 1;
+         let progress = timing(timeFraction);
+         draw(progress);
+         if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+         } else {
+            resolve();
+         }
+      });
+   })
+}
+
+function show(elem) {
+   return animatePromise({
+      duration: 400,
+      draw: function draw(progress) {
+         elem.style.opacity = progress;
+      },
+      timing: function circ(timeFraction) {
+         return timeFraction;
+      }
+   })
+}
+
+function hidden(elem) {
+   return animatePromise({
+      duration: 400,
+      draw: function draw(progress) {
+         elem.style.opacity = 1 - progress;
+      },
+      timing: function circ(timeFraction) {
+         return timeFraction;
+      }
+   });
+}
